cac2466bcff7b2737c918a8e8dbae77c34df715a
fajrulaslim <fajrul.aslim@gmail.com>
2024-02-29 18:00:29 +0700

    kuis

--- routes/frontend/kuis.js
+++ routes/frontend/kuis.js
@@ -1,11 +1,42 @@
 const express = require('express');
 const router = express.Router();
 const Kuis = require('../../models/kuis');
+const Level = require('../../models/level');
 
 router.get('/', async (req, res) => {
   try {
     const kuises = await Kuis.findAll();
-    res.render('kuis', { kuises });
+    const levels = await Level.findAll();
+    res.render('kuis', { kuises, levels });
+  } catch (error) {
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+router.post('/add', async (req, res) => {
+  try {
+    const createUser = await Kuis.create(req.body)
+    res.redirect(`/`);
+  } catch (error) {
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+router.post('/edit/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const updatedUser = await User.update(req.body, { where: { id } })
+    res.redirect(`/`);
+  } catch (error) {
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+router.post('/delete/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deleteUser = await User.destroy({ where: { id } })
+    res.redirect(`/`);
   } catch (error) {
     res.status(500).send('Internal Server Error');
   }
