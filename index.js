4d35d7774fa725a2c1a85e5a31b3275cd0ed24c6
fajrulaslim <fajrul.aslim@gmail.com>
2024-03-02 11:46:25 +0700

    cors

--- index.js
+++ index.js
@@ -28,6 +28,7 @@ app.use(
       "https://www.pedemath.com",
       "https://pedemath.vercel.app",
       "http://localhost:50000",
+      "http://127.0.0.1:5500"
     ],
     methods: "GET,POST,PUT,DELETE",
     credentials: true,
